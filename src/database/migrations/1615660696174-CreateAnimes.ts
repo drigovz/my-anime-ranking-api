901fdb693279ea2eebe186dd2c5dc4bb83fdd435
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-03-13 16:13:33 -0300

    feat: confg code of first migration

--- src/database/migrations/1615660696174-CreateAnimes.ts
+++ src/database/migrations/1615660696174-CreateAnimes.ts
@@ -1,10 +1,41 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateAnimes1615660696174 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "animes",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "title",
+                        type: "varchar"
+                    },
+                    {
+                        name: "description",
+                        type: "varchar"
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    },
+                    {
+                        name: "note",
+                        type: "int",
+                        default: 0
+                    }
+                ]
+            })
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("animes");
     }
 }
