a687cdbb81a1b52c90fae4399bb297cf418a5dbb
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-03-13 14:43:08 -0300

    feat: improvements in database connection

---
+++ ormconfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "type":"postgres",
+    "host": process.env.POSTGRES_HOST,
+    "port": process.env.POSTGRES_PORT || "5432",
+    "username": process.env.POSTGRES_USER,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DATABASE
+}
